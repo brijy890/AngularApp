4b97f1fe8bb09364d30885f53b88a6b57bf1c804
Brij <brijbhushan.yadav@ambab.com>
2018-11-05 00:18:28 +0530

    course project setup

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipes } from '../recipes.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+	recipes: Recipes[] = [
+		new Recipes('A Test Recipes', 'This is a simple recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+		new Recipes('A Test Recipes', 'This is a simple recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+	];
 
   constructor() { }
 
